a3c44d458651a895d0d5f619fb23e1fb58100879
Anand Yadav <anand.yadav@nagarro.com>
2024-06-06 16:20:18 +0530

    Initial Commit

---
+++ SwiftDataDemo/SwiftDataDemo/SwiftDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataDemoApp.swift
+//  SwiftDataDemo
+//
+//  Created by Anand Yadav on 06-06-2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
